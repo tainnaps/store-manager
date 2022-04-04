7776939ae44af3215c01ce3e8df73a563b4b6215
Taina Santana <tainnaps@gmail.com>
2022-04-04 15:40:07 -0300

    test: getAll sales service unity test

---
+++ test/unit/services/salesServices.js
@@ -0,0 +1,71 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const SalesModels = require('../../../models/sales');
+const SalesServices = require('../../../services/sales');
+
+describe('SalesServices', () => {
+  describe('Get all sales', () => {
+    describe('when there are registered sales', () => {
+      const fakeSales = [
+        {
+          saleId: 1,
+          date: '2022-04-03T05:09:19.000Z',
+          productId: 1,
+          quantity: 5
+        },
+        {
+          saleId: 2,
+          date: '2022-04-03T05:09:19.000Z',
+          productId: 3,
+          quantity: 15
+        },
+      ];
+
+      before(() => {
+        const fakeResult = [
+          {
+            sale_id: 1,
+            date: '2022-04-03T05:09:19.000Z',
+            product_id: 1,
+            quantity: 5
+          },
+          {
+            sale_id: 2,
+            date: '2022-04-03T05:09:19.000Z',
+            product_id: 3,
+            quantity: 15
+          },
+        ];
+
+        sinon.stub(SalesModels, 'getAll').resolves(fakeResult);
+      });
+
+      after(() => {
+        SalesModels.getAll.restore();
+      });
+
+      it('should return an array of sales', async () => {
+        const sales = await SalesServices.getAll();
+
+        expect(sales).to.be.an('array');
+        expect(sales).to.deep.equal(fakeSales);
+      });
+    });
+
+    describe('when there are no registered products', () => {
+      before(() => {
+        sinon.stub(SalesModels, 'getAll').resolves([]);
+      });
+
+      after(() => {
+        SalesModels.getAll.restore();
+      });
+
+      it('should return an empty array', async () => {
+        const products = await SalesServices.getAll();
+
+        expect(products).to.be.an('array').that.is.empty;
+      });
+    });
+  });
+});
